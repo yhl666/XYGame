c7d7f7c14a287ef0a18c21550f75153c5d1448db
梦想游戏人 <771911064@qq.com>
2017-02-12 00:42:09 +0800

    refactoring app

---
+++ UnityClient/Assets/Logic/Scripts/GameApp/TownApp.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+public class TownApp : AppBase
+{
+    private bool init = false;
+    // Use this for initialization
+
+    public override bool Init()
+    {
+        if (init) return true;
+
+        base.Init();
+        Application.targetFrameRate = 40;
+        init = true;
+        Debug.Log("new App");
+
+        this.worldMap = ModelMgr.Create<LogicWorldMap>();
+
+
+        string seed = (new System.Random(Convert.ToInt32((DateTime.Now - new DateTime(1970, 1, 1, 8, 0, 0)).TotalSeconds))).Next(9999).ToString();
+
+
+        RpcClient.ins.SendRequest("services.login", "login", "name:" + seed + ",", (string msg) =>
+        {
+            if (msg == "")
+            {
+                Debug.Log("error");
+            }
+            else
+            {
+                HashTable kv = Json.Decode(msg);
+                Debug.Log(" login " + kv["res"]);
+            }
+
+
+        });
+
+        return true;
+    }
+    BaseHero self = null;
+    public override void OnEnter()
+    {
+        base.OnEnter();
+
+
+
+    }
+
+
+    int tick = 0;
+    public override void UpdateMS()
+    {
+        tick++;
+        ModelMgr.ins.UpdateMS();
+        ViewMgr.ins.UpdateMS();
+        if (HeroMgr.ins.self == null) return;
+
+        var random =new  System.Random();
+
+        /*if(random.Next(40)==10)
+        {
+            RpcClient.ins.SendRequest("services.room", "new_position", "no:" + HeroMgr.ins.self.no + ",x:" + random.NextDouble()*24 +
+              ",y:" + random.NextDouble() * 2.5 + ",", (string msg) =>
+              {
+                  if (msg != "")
+                  {
+
+
+                      Debug.Log(" new postion ok ");
+                  }
+
+
+              });
+        }*/
+        Vector3 pos = Input.mousePosition;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            //   Debug.Log(pos.x + "          " + pos.y);
+            Vector3 pos_world = Camera.main.ScreenToWorldPoint(pos);
+
+
+            //    Debug.Log(pos_world.x + "          " + pos_world.y);
+
+            //     ---  BaseHeroMgr.ins.self.eventDispatcher.PostEvent(Events.ID_LOGIC_NEW_POSITION, new Vector2(pos_world.x, pos_world.y));
+
+
+            RpcClient.ins.SendRequest("services.room", "new_position", "no:" + HeroMgr.ins.self.no + ",x:" + pos_world.x.ToString() +
+                 ",y:" + pos_world.y.ToString() + ",", (string msg) =>
+            {
+                if (msg != "")
+                {
+
+
+                    Debug.Log(" new postion ok ");
+                }
+
+
+            });
+
+        }
+
+
+
+    }
+
+
+}
---
+++ UnityClient/Assets/Scripts/GameApp/AppMgr.cs
@@ -0,0 +1,139 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class AppMgr : GAObject
+{
+    public static AppMgr ins
+    {
+        get
+        {
+            return AppMgr.GetInstance();
+        }
+    }
+
+    private static AppMgr _ins = null;
+
+    public static AppMgr GetInstance()
+    {
+        if (_ins == null)
+        {
+            _ins = new AppMgr();
+            _ins.Init();
+        }
+        return _ins;
+    }
+
+    //------------------------------------override 
+    public override bool Init()
+    {
+        base.Init();
+
+        return true;
+    }
+
+    public override void OnEnter()
+    {
+        base.OnEnter();
+    }
+
+    public override void OnExit()
+    {
+        base.OnExit();
+    }
+    public override void OnDispose()
+    {
+        base.OnDispose();
+    }
+    public override void OnEvent(int type, object userData)
+    {
+
+    }
+
+
+    public override void Update()
+    {
+        base.Update();
+        if (current_app != null) current_app.Update();
+        AutoReleasePool.ins.Clear();
+
+    }
+
+    public override void UpdateMS()
+    {
+        base.UpdateMS();
+        if (current_app != null) current_app.UpdateMS();
+        AutoReleasePool.ins.Clear();
+
+    }
+
+
+
+    // ----------------controller 
+
+    public void LoadApp<T>() where T : new()
+    {
+        this.Clear();
+        this.Push(Create<T>());
+    }
+    public void ReplaceApp<T>() where T : new()
+    {
+        this.Pop();
+        this.Push(Create<T>());
+    }
+    public void PushApp<T>() where T : new()
+    {
+        this.Push(Create<T>());
+    }
+    public void PopApp()
+    {
+        this.Pop();
+    }
+
+    public static AppBase Create<T>() where T : new()
+    {
+        AppBase ret = new T() as AppBase;
+        ret.Init();
+        return ret;
+    }
+    public static AppBase GetCurrentApp()
+    {
+        return current_app;
+    }
+    public static T GetCurrentApp<T>() where T : AppBase
+    {
+        return (T)current_app;
+    }
+
+    //-------------private helper function
+
+    private void Push(AppBase app)
+    {
+        app.OnEnter();
+        stack.Push(app);
+        current_app = app;
+    }
+    private void Pop()
+    {
+        if (this.stack.Count <= 0)
+        {
+            current_app = null;
+            return;
+        }
+        AppBase app = this.stack.Pop() as AppBase;
+        app.OnExit();
+    }
+    private void Clear()
+    {
+        while (this.stack.Count > 0)
+        {
+            this.Pop();
+        }
+        this.stack.Clear();
+        current_app = null;
+    }
+
+    private static AppBase current_app = null;
+    private Stack stack = new Stack();
+
+}
