9facc42ccd7b9fe41d1f0cb7d5a956ce99243291
梦想游戏人 <771911064@qq.com>
2017-02-13 17:02:44 +0800

    add login

--- LogicServer/server/scripts/base/xygame/services/login.lua
+++ LogicServer/server/scripts/base/xygame/services/login.lua
@@ -20,33 +20,67 @@ global_base_heros = { };
 function t.login(ctx, msg, cb)
     local kv = json.decode(msg);
 
-    -- 新连接请求
-    remote.request_client(ctx, "Room", "SelfEnterRoom", "no:" .. kv["name"] .. ",", function(msg)
+    local account = kv["account"];
+    local pwd = kv["pwd"];
 
-        if msg == "timeout" then return end;
-        --- 响应失败 忽略
+    redis.get("login-" .. account, function(msg)
 
-        print("hero join game no=" .. kv["name"]);
+        if msg == "" then
+            cb("ret:账户不存在,"); return;
+        end
+        local kvv = json.decode(msg);
 
-        -- 响应成功后 添加到table里面
-        table.insert(global_base_heros, hero.create(ctx, kv["name"]));
+        if pwd == kvv["pwd"] then
+            -- 登陆成功
+            cb("ret:ok,name:" .. kvv["name"] .. ",");
+        else
+            -- 账户密码错误
+            cb("ret:密码或账户错误,");
+        end
+
+
+
 
     end );
 
-
-    
-   redis.get("ssqgqwegss"  ,function (msg)
-       print(msg);
-    end);
-
-       
-   redis.set("ssss"  ,"ewgewewghewewhew",function (msg)
-       print(msg);
-    end);
-
-
-    cb("ret:ok,");
 end
+
+
+
+
+
+function t.register(ctx, msg, cb)
+    local kv = json.decode(msg);
+    local name = kv["name"];
+    local account = kv["account"];
+    local pwd = kv["pwd"];
+
+
+    -- 先查询账户存在否
+
+    redis.get("login-" .. account, function(msg)
+
+        if msg == "" then
+            --- "账户不存在
+            redis.set("login-" .. account, "pwd:" .. pwd .. "," .. "name:" .. name .. ",", function(msg)
+
+                if msg == "ok" then
+                    cb("ret:ok"); return;
+                end
+                cb("ret:服务器内部错误，注册失败,");
+            end );
+        else
+
+            cb("ret:账户已存在,");
+
+        end
+
+
+    end );
+
+end
+
+
 
 
 return t;
