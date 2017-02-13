f72bace861c689367d7a00cef0419b2602e7fbea
梦想游戏人 <771911064@qq.com>
2017-02-13 15:06:43 +0800

    no commit message

---
+++ LogicServer/server/scripts/common/xygame/Template.lua
@@ -0,0 +1,21 @@
+
+--[[
+
+* Author: caoshanshan
+* Email: me@dreamyouxi.com
+
+]]
+
+
+local t = { }
+
+local log = require("log"):new("login")
+local remote = require("base.remote");
+local hero = require("model.base_hero")
+
+function t.login(ctx, msg, cb)
+
+end
+
+
+return t;
