efa935ab96b16c57f895974b0efc50143e98d3b3
梦想游戏人 <771911064@qq.com>
2017-02-16 00:00:06 +0800

    no commit message

---
+++ UnityClient/Assets/AutoFlitScreen.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoFlitScreen : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        trans = this.GetComponent<RectTransform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        trans.sizeDelta = new Vector2(Screen.width, Screen.height);
+        trans.localPosition = new Vector3(-Screen.width/2.0F,- Screen.height/2.0f, 0);
+    }
+
+
+    RectTransform trans = null;
+}
