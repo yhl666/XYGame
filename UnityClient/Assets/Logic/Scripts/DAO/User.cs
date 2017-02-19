cf46dbd5608f3f878465d3f2422eb705b149f7f9
梦想游戏人 <771911064@qq.com>
2017-02-19 11:27:34 +0800

    no commit message

--- UnityClient/Assets/Logic/Scripts/DAO/User.cs
+++ UnityClient/Assets/Logic/Scripts/DAO/User.cs
@@ -20,13 +20,14 @@ public class DaoBase
 namespace DAO
 {
 
-    public class User : DaoBase
+    public sealed class User : DaoBase
     {
         public int no;
         public string name;
         public int level;
         public string time = "0";
         public string type = "2";
+
 
 
         public void SetHashTable(HashTable kv)
@@ -44,7 +45,7 @@ namespace DAO
 
         public override string ToJson()
         {
-            return "no:" + no.ToString() + ",name:" + name + ",level:" + level.ToString() + ",type:" + type .ToString() +  ",time:" + time .ToString()+  ",";
+            return "no:" + no.ToString() + ",name:" + name + ",level:" + level.ToString() + ",type:" + type.ToString() + ",time:" + time.ToString() + ",";
         }
         public static User Create(string json)
         {
@@ -69,4 +70,48 @@ namespace DAO
 
         private User() { }
     }
+
+
+
+
+
+
+
+    public sealed class Equip : DaoBase
+    {
+        public string id;//全局id
+        public string ownner;//拥有者id
+        public string no;//装备id
+        public int level; // 等级
+        public int exp;
+
+
+        //
+        public string type;
+        public string name;
+        public string brief;
+        public string detail;
+
+        public int damage;//伤害加成
+        public int defend;//防御加成
+        public int mp;//魔法加成
+        public int hp;//气血加成
+
+        public float crits_ratio = 10.0f;//暴击率 加成 //0~100 %
+        public float crits_damage = 10.0f;//暴击伤害 加成表示200% 倍数
+        public float cure=0.0f;//治疗加成
+
+
+
+
+
+
+        public ArrayList buffers = new ArrayList();//buffers id
+
+    }
+
+
+
+
+
 }
