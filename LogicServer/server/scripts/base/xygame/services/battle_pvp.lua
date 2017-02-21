e38b5bc164be03d96dd24a13babf003c0e79d6a3
梦想游戏人 <771911064@qq.com>
2017-02-21 11:18:11 +0800

    添加pvp

---
+++ LogicServer/server/scripts/base/xygame/services/battle_pvp.lua
@@ -0,0 +1,27 @@
+
+--[[
+
+* Author:  
+* Email:  
+
+
+--{no:1,no_target:2,}no我的id no_target 我要战的id
+--request_pvp_v1(ctx, msg, cb)
+
+end
+]]
+
+
+local t = { }
+
+local log = require("log"):new("login")
+local remote = require("base.remote");
+local hero = require("model.base_hero")
+--[Common]
+--{no:1,no_target:2,}
+function t.request_pvp_v1(ctx, msg, cb)
+
+end
+
+
+return t;
