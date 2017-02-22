300c9e0212f22b62b669053479c65ad08ea217a1
梦想游戏人 <771911064@qq.com>
2017-02-22 17:41:33 +0800

    no commit message

---
+++ LogicServer/server/scripts/common/xygame/base/client_server_handler.lua
@@ -0,0 +1,43 @@
+
+--[[
+--外部不需要访问该文件接口，请用remote.lua 来访问
+* Author: caoshanshan
+* Email: me@dreamyouxi.com
+
+]]
+
+local t  = { }
+
+ 
+
+local client_room_map = { }
+
+
+function t.handle_client_server(room_info)
+ 
+    local kv = json.decode(room_info);
+    local key = "pvproom_id:" .. kv["pvproom_id"] .. ",p1:" .. kv["p1"] .. ",p2:" .. kv["p2"] .. ",";
+
+    local cb = client_room_map[key];
+
+
+    cb(room_info);
+
+end
+
+
+function t.add_client_server(room_info, cb)
+
+    client_room_map[room_info] = cb;
+    print("reg " .. room_info .. tostring(client_room_map[tostring( room_info)]));
+
+end
+
+
+-- "pvproom_id:2,p1:1,p2:2,"
+function t.request_client_server(room_info, cb)
+    t.add_client_server(room_info, cb);
+    client_server.send(room_info);
+
+end
+return t
