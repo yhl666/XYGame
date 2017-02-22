ad5a40d8fea1f1a3eae1d5182a872ab6b03f9151
梦想游戏人 <771911064@qq.com>
2017-02-22 22:11:58 +0800

    no commit message

---
+++ UnityClient/Assets/ClientServerText.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ClientServerText : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Application.targetFrameRate = 0xfffffff;
+
+        this.GetComponent<Text>().text = "队列剩余请求:" + ClientServerApp.ins.GetCurrentQueueCount();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
