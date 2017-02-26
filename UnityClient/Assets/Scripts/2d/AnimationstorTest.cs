d3d97e2de798e5a0441783fecd023a27c79ca5f0
梦想游戏人 <771911064@qq.com>
2017-02-26 21:52:05 +0800

    no commit message

---
+++ UnityClient/Assets/Scripts/2d/AnimationstorTest.cs
@@ -0,0 +1,68 @@
+﻿/*
+* Author:  caoshanshan
+* Email:   me@dreamyouxi.com
+
+ */
+using UnityEngine;
+using System.Collections;
+
+public class AnimationstorTest : MonoBehaviour
+{
+
+
+    [SerializeField]
+    public string file;
+
+    [SerializeField]
+    public int FrameDelay;
+
+    public Animations ani = null;
+    void Awake()
+    {
+        this.Init();
+    }
+
+    public void Update()
+    {
+ 
+
+        if (ani == null)
+        {
+            this.Init();
+        }
+     ///   if (ani != null)
+        ani.UpdateMS();
+    }
+
+    void OnDestroy()
+    {
+        if (ani != null)
+            ani.Dispose();
+
+
+
+    }
+    public bool Init()
+    {
+        string name = Utils.GetFileName(file);
+
+        ani = AnimationsCache.ins.GetAnimations(name);
+        if (ani == null)
+        {
+            ani = AnimationsCache.ins.AddAnimatons(name, Animations.CreateWithFile(file));
+         ani.Init();
+         ani.Run();
+        }
+
+        ani.target = this.GetComponent<SpriteRenderer>();
+        ani.perFrame = FrameDelay;
+
+        return true;
+    }
+    public static Animationstor Create(string file)
+    {
+
+        return null;
+
+    }
+}
