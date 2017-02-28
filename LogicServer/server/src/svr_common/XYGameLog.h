eb22b8ccc88ed5fb05e50ee94d4459d0a34ddb96
梦想游戏人 <771911064@qq.com>
2017-02-28 19:53:56 +0800

    添加记录玩家关键操作，可用于GM系统分析 处理 接口

---
+++ LogicServer/server/src/svr_common/XYGameLog.h
@@ -0,0 +1,53 @@
+﻿#ifndef __BASE_SERVER_HEAD__CLIENTSERVER
+#define __BASE_SERVER_HEAD__CLIENTSERVER
+
+
+
+#include <string>
+#include <thread>
+#include <mutex>
+#include <queue>
+#include <memory>
+#include "timer_queue.h"
+struct lua_State;
+ 
+class XYGameLog
+{//记录玩家关键操作，可用于GM系统分析 处理
+public:
+
+	static XYGameLog *GetInstance()
+	{
+		static XYGameLog ins;
+		return &ins;//thread safe with c++11
+	}
+private:
+
+public:
+//记录一条日志接口
+	void Log(const std::string &file,const std::string & info);
+
+	void BindLua(lua_State *l);
+
+private:
+	XYGameLog();
+	//定期持久化到文件
+	void SyncToFile();
+	std::unordered_map <std::string,std::string> hash;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif		// __BASE_SERVER_HEAD__
