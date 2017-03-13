4d063c777076467df04aa0a07a01e70c9eca2849
梦想游戏人 <771911064@qq.com>
2017-03-13 17:31:08 +0800

    添加客户端测试代码

---
+++ UnityClient/Assets/ClientTestApp.cs
@@ -0,0 +1,85 @@
+﻿
+/*
+* Author:  caoshanshan
+* Email:   me@dreamyouxi.com
+
+ */
+using UnityEngine;
+using System.Collections;
+using System;
+using System.Threading;
+using System.Net.Sockets;
+using System.Timers;
+
+
+public class ClientTestApp : GAObject
+{
+    public static ClientTestApp ins
+    {
+        get
+        {
+            return ClientTestApp.GetInstance();
+        }
+    }
+
+    private static ClientTestApp _ins = null;
+
+    public static ClientTestApp GetInstance()
+    {
+        if (_ins == null)
+        {
+            _ins = new ClientTestApp();
+            _ins.Init();
+            _ins.OnEnter();
+        }
+        return _ins;
+    }
+
+
+
+
+    public override bool Init()
+    {
+
+        base.Init();
+
+        return true;
+    }
+
+
+    public override void OnEnter()
+    {
+        base.OnEnter();
+
+        for (int i = 0; i < 1000000; i++)
+        {
+            TimerQueue.ins.AddTimer(10.0f, () =>
+            {
+                Debug.Log("1111111");
+
+            });
+
+        }
+
+    }
+    public override void OnDispose()
+    {
+
+
+    }
+
+    public override void OnEvent(int type, object userData)
+    {
+
+    }
+
+    public override void UpdateMS()
+    {
+        EventDispatcher.ins.Update();
+
+
+    }
+
+
+
+}
---
+++ UnityClient/Assets/ClientTestHost.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClientTestHost : MonoBehaviour
+{
+    static ClientTestApp app = null;
+    // Use this for initialization
+    void Awake()
+    {
+      ///  GameObject.DontDestroyOnLoad(this);
+        if (app == null)
+        {
+            app = ClientTestApp.ins;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        app.UpdateMS();
+    }
+    void OnDestroy()
+    {
+       // app.Dispose();
+    }
+}
