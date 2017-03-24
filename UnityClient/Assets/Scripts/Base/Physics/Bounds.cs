2f88445d141d5a38391deacaa372b21fb519cb2c
梦想游戏人 <771911064@qq.com>
2017-03-24 11:12:53 +0800

    添加2D 3D碰撞盒

---
+++ UnityClient/Assets/Scripts/Base/Physics/Bounds.cs
@@ -0,0 +1,149 @@
+﻿/*
+* Author:  caoshanshan
+* Email:   me@dreamyouxi.com
+way 1.use unity's physicsX to process collider
+way 2. 
+ */
+using UnityEngine;
+using System.Collections;
+using System;
+
+/// <summary>
+/// wrapper of bounds
+/// </summary>
+public class BoundsImpl
+{
+    Bounds impl;
+
+    public const float DEFAULT_Z = 1.0f;
+    private BoundsImpl(Vector3 center, Vector3 size)
+    {
+        impl = new Bounds(center, size);
+    }
+    private BoundsImpl(Vector2 center, Vector2 size)
+    {
+        impl = new Bounds(new Vector3(center.x, center.y, DEFAULT_Z), new Vector3(size.x, size.y, DEFAULT_Z));
+    }
+    private BoundsImpl()
+    {
+        impl = new Bounds();
+    }
+    public static BoundsImpl Create()
+    {
+        BoundsImpl ret = new BoundsImpl();
+        return ret;
+    }
+    public static BoundsImpl Create(Vector3 center, Vector3 size)
+    {
+        BoundsImpl ret = new BoundsImpl(center, size);
+        return ret;
+    }
+    public static BoundsImpl Create(Vector2 center, Vector2 size)
+    {
+        BoundsImpl ret = new BoundsImpl(center, size);
+        return ret;
+    }
+    public static bool operator !=(BoundsImpl lhs, BoundsImpl rhs)
+    {
+        return lhs.impl != rhs.impl;
+    }
+    public static bool operator ==(BoundsImpl lhs, BoundsImpl rhs)
+    {
+        return lhs.impl == rhs.impl;
+    }
+
+    // 摘要: 
+    //     The center of the bounding box.
+    public Vector3 center
+    {
+        get
+        {
+            return impl.center;
+        }
+        set
+        {
+            impl.center = value;
+        }
+    }
+    //
+    // 摘要: 
+    //     The extents of the box. This is always half of the size.
+    public Vector3 extents
+    {
+        get
+        {
+            return impl.extents;
+        }
+        set
+        {
+            impl.extents = value;
+        }
+    }
+    //
+    // 摘要: 
+    //     The maximal point of the box. This is always equal to center+extents.
+    public Vector3 max
+    {
+        get
+        {
+            return impl.max;
+        }
+        set
+        {
+            impl.max = value;
+        }
+    }
+    //
+    // 摘要: 
+    //     The minimal point of the box. This is always equal to center-extents.
+    public Vector3 min
+    {
+        get
+        {
+            return impl.min;
+        }
+        set
+        {
+            impl.min = value;
+        }
+    }
+    //
+    // 摘要: 
+    //     The total size of the box. This is always twice as large as the extents.
+    public Vector3 size
+    {
+        get
+        {
+            return impl.size;
+        }
+        set
+        {
+            impl.size = value;
+        }
+    }
+ 
+    public bool Contains(Vector3 point)
+    {
+        return impl.Contains(point);
+    }
+    public bool Contains(Vector2 point)
+    {
+        return impl.Contains(new Vector3(point.x, point.y, 0f));
+    }
+    public bool IntersectRay(Ray ray)
+    {
+        return impl.IntersectRay(ray);
+    }
+    public bool IntersectRay(Ray ray, out float distance)
+    {
+        return impl.IntersectRay(ray, out distance);
+    }
+    public bool Intersects(BoundsImpl bounds)
+    {
+        return impl.Intersects(bounds.impl);
+    }
+    public float SqrDistance(Vector3 point)
+    {
+        return impl.SqrDistance(point);
+    }
+}
