a6fd15b5b090a8b519789f76ecde24a4bd85d031
JefferyCoco <705435286@qq.com>
2017-04-18 15:49:59 +0800

    提交了技能图标的配置

---
+++ UnityClient/Assets/Scripts/Data/SkillImagePath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+    class SkillImagePath:MonoBehaviour
+    {
+        public Sprite skill1;
+        public Sprite skill2;
+        public Sprite skill3;
+        public static SkillImagePath ins;
+
+        void Awake()
+        {
+            ins = this;
+        }
+    }
+
